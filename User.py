955c09052b6baea523030820aa63552b5e7c2a67
Leo-Brd <leobordet.pro@gmail.com>
2024-03-14 11:11:57 +0100

    construct_user

--- User.py
+++ User.py
@@ -1,1 +1,16 @@
 # Fichier User
+
+class User:
+
+    def __init__(self, User_id : int, Firstname : str, Name : str, Age: int, Email : str, Username : str, Password : str, Rental_list : list) -> None:
+        self.__User_id = User_id
+        self.__Firstname = Firstname
+        self.__Name = Name
+        self.__Age = Age
+        self.__Email = Email
+        self.__Username = Username
+        self.__Password = Password
+        self.__Rental_list = Rental_list
+
+    
+
